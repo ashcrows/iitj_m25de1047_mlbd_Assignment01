bbdbcaee31d2977af3e46e595fcad900bda1f87a
ashishsinha <asinha@guardanthealth.com>
2026-02-15 21:50:03 +0530

    Update .gitignore to exclude all .txt files in q4_wordcount/input/D184MB except 200.txt; refactor reduce method in WordCount class for consistency

--- q4_wordcount/src/WordCount.java
+++ q4_wordcount/src/WordCount.java
@@ -52,13 +52,13 @@ public class WordCount {
 
     @Override
     public void reduce(Text key, Iterable<IntWritable> values, Context context)
-        throws IOException, InterruptedException {
+            throws IOException, InterruptedException {
 
-      int sum = 0;
-      for (IntWritable val : values) {
-        sum += val.get();
-      }
-      context.write(key, new IntWritable(sum));
+        int sum = 0;
+        for (IntWritable val : values) {
+            sum += val.get();
+        }
+        context.write(key, new IntWritable(sum));
     }
   }
 
